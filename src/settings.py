1ec8ef32908b24b9ba433c7f521dc8406fbc183c
erastov-alex <erastov.alex@gmail.com>
2023-11-06 15:02:42 +0300

    add img preview

--- src/settings.py
+++ src/settings.py
@@ -34,7 +34,7 @@ if RELEASE_DATE is None:
 HOMEPAGE_URL: str = "https://github.com/LAR/lincolnbeet_dataset"
 # e.g. "https://some.com/dataset/homepage"
 
-PREVIEW_IMAGE_ID: int = None
+PREVIEW_IMAGE_ID: int = 8063666
 # This should be filled AFTER uploading images to instance, just ID of any image.
 
 GITHUB_URL: str = "https://github.com/dataset-ninja/lincoln-beet"

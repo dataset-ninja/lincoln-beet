c5bb3a164c0afbaf5ca3bf6fe11916b0d39689a2
erastov-alex <erastov.alex@gmail.com>
2023-11-06 14:45:45 +0300

    upd gitignore, convert, settings

--- src/convert.py
+++ src/convert.py
@@ -1,9 +1,12 @@
 import supervisely as sly
 import os
+import csv
+from collections import defaultdict
 from dataset_tools.convert import unpack_if_archive
 import src.settings as s
 from urllib.parse import unquote, urlparse
-from supervisely.io.fs import get_file_name, get_file_size
+from supervisely.io.json import load_json_file
+from supervisely.io.fs import get_file_name, get_file_name_with_ext
 import shutil
 
 from tqdm import tqdm
@@ -69,17 +72,124 @@ def count_files(path, extension):
 def convert_and_upload_supervisely_project(
     api: sly.Api, workspace_id: int, project_name: str
 ) -> sly.ProjectInfo:
-    ### Function should read local dataset and upload it to Supervisely project, then return project info.###
-    raise NotImplementedError("The converter should be implemented manually.")
+    
+    dataset_path = os.path.join("all_fields_lincolnbeet","all")
+    image_characteristics_path = os.path.join("all_fields_lincolnbeet","image_characteristics.csv")
+    train_anns_path = os.path.join("all_fields_lincolnbeet","json_train_set.json")
+    test_anns_path = os.path.join("all_fields_lincolnbeet","json_test_set.json")
+    val_anns_path = os.path.join("all_fields_lincolnbeet","json_val_set.json")
+    batch_size = 300
 
-    # dataset_path = "/local/path/to/your/dataset" # general way
-    # dataset_path = download_dataset(teamfiles_dir) # for large datasets stored on instance
-
-    # ... some code here ...
-
-    # sly.logger.info('Deleting temporary app storage files...')
-    # shutil.rmtree(storage_dir)
-
-    # return project
+    ds_name_to_anns = {"train": train_anns_path, "val": val_anns_path, "test": test_anns_path}
 
 
+    def create_ann(image_path):
+        labels = []
+
+        tags_data = im_name_to_tags[get_file_name_with_ext(image_path)]
+        item_density_value = tags_data[0]
+        item_density = sly.Tag(item_density_meta, value=item_density_value)
+
+        bboxes_occupation_value = float(tags_data[1])
+        bboxes_occupation = sly.Tag(bboxes_occupation_meta, value=bboxes_occupation_value)
+
+        average_size_value = float(tags_data[2])
+        average_size = sly.Tag(average_size_meta, value=average_size_value)
+
+        average_occlusion_value = float(tags_data[3])
+        average_occlusion = sly.Tag(average_occlusion_meta, value=average_occlusion_value)
+
+        # image_np = sly.imaging.image.read(image_path)[:, :, 0]
+        img_height = image_name_to_shape[get_file_name_with_ext(image_path)][0]  # image_np.shape[0]
+        img_wight = image_name_to_shape[get_file_name_with_ext(image_path)][1]  # image_np.shape[1]
+
+        ann_data = image_name_to_ann_data[get_file_name_with_ext(image_path)]
+        for curr_ann_data in ann_data:
+            category_id = curr_ann_data[0]
+
+            bbox_coord = curr_ann_data[1]
+            rectangle = sly.Rectangle(
+                top=int(bbox_coord[1]),
+                left=int(bbox_coord[0]),
+                bottom=int(bbox_coord[1] + bbox_coord[3]),
+                right=int(bbox_coord[0] + bbox_coord[2]),
+            )
+            label_rectangle = sly.Label(rectangle, idx_to_obj_class[category_id])
+            labels.append(label_rectangle)
+
+        return sly.Annotation(
+            img_size=(img_height, img_wight),
+            labels=labels,
+            img_tags=[item_density, bboxes_occupation, average_size, average_occlusion],
+        )
+
+
+    sugar = sly.ObjClass("sugar_beet", sly.Rectangle)
+    weed = sly.ObjClass("weed", sly.AnyGeometry)
+    idx_to_obj_class = {0: sugar, 1: weed}
+
+    item_density_meta = sly.TagMeta("item_density", sly.TagValueType.ANY_STRING)
+    bboxes_occupation_meta = sly.TagMeta("bboxes_occupation", sly.TagValueType.ANY_NUMBER)
+    average_size_meta = sly.TagMeta("average_relative_size", sly.TagValueType.ANY_NUMBER)
+    average_occlusion_meta = sly.TagMeta("average_levels_occlusion", sly.TagValueType.ANY_NUMBER)
+    project = api.project.create(workspace_id, project_name, change_name_if_conflict=True)
+    meta = sly.ProjectMeta(
+        obj_classes=[sugar, weed],
+        tag_metas=[
+            item_density_meta,
+            bboxes_occupation_meta,
+            average_size_meta,
+            average_occlusion_meta,
+        ],
+    )
+    api.project.update_meta(project.id, meta.to_json())
+
+    im_name_to_tags = {}
+    with open(image_characteristics_path, "r") as file:
+        csvreader = csv.reader(file)
+        for idx, row in enumerate(csvreader):
+            if idx == 0:
+                continue
+            im_name_to_tags[row[2]] = row[-4:]
+
+
+    for ds_name, ann_path in ds_name_to_anns.items():
+        dataset = api.dataset.create(project.id, ds_name, change_name_if_conflict=True)
+
+        ann = load_json_file(ann_path)
+
+        image_id_to_name = {}
+        image_name_to_shape = {}
+        image_name_to_ann_data = defaultdict(list)
+
+        for curr_image_info in ann["images"]:
+            image_id_to_name[curr_image_info["id"]] = curr_image_info["file_name"]
+            image_name_to_shape[curr_image_info["file_name"]] = (
+                int(curr_image_info["height"]),
+                int(curr_image_info["width"]),
+            )
+
+        for curr_ann_data in ann["annotations"]:
+            image_id = curr_ann_data["image_id"]
+            image_name_to_ann_data[image_id_to_name[image_id]].append(
+                [curr_ann_data["category_id"], curr_ann_data["bbox"]]
+            )
+
+        images_names = list(image_name_to_ann_data.keys())
+
+        progress = sly.Progress("Create dataset {}".format(ds_name), len(images_names))
+
+        for img_names_batch in sly.batched(images_names, batch_size=batch_size):
+            images_pathes_batch = [
+                os.path.join(dataset_path, image_path) for image_path in img_names_batch
+            ]
+
+            img_infos = api.image.upload_paths(dataset.id, img_names_batch, images_pathes_batch)
+            img_ids = [im_info.id for im_info in img_infos]
+
+            anns_batch = [create_ann(image_path) for image_path in images_pathes_batch]
+            api.annotation.upload_anns(img_ids, anns_batch)
+
+            progress.iters_done_report(len(img_names_batch))
+
+    return project
--- src/settings.py
+++ src/settings.py
@@ -13,37 +13,37 @@ from dataset_tools.templates import (
 ##################################
 # * Before uploading to instance #
 ##################################
-PROJECT_NAME: str = None
-PROJECT_NAME_FULL: str = None
+PROJECT_NAME: str = "Lincoln Beet"
+PROJECT_NAME_FULL: str = "Lincoln Beet"
 HIDE_DATASET = True  # set False when 100% sure about repo quality
 
 ##################################
 # * After uploading to instance ##
 ##################################
-LICENSE: License = None
-APPLICATIONS: List[Union[Industry, Domain, Research]] = None
-CATEGORY: Category = None
+LICENSE: License = License.Unknown()
+APPLICATIONS: List[Union[Industry, Domain, Research]] = [Industry.Agricultural()]
+CATEGORY: Category = Category.Agriculture()
 
-CV_TASKS: List[CVTask] = None
-ANNOTATION_TYPES: List[AnnotationType] = None
+CV_TASKS: List[CVTask] = [CVTask.ObjectDetection()]
+ANNOTATION_TYPES: List[AnnotationType] = [AnnotationType.ObjectDetection()]
 
-RELEASE_DATE: Optional[str] = None  # e.g. "YYYY-MM-DD"
+RELEASE_DATE: Optional[str] = "2021-09-28"  # e.g. "YYYY-MM-DD"
 if RELEASE_DATE is None:
     RELEASE_YEAR: int = None
 
-HOMEPAGE_URL: str = None
+HOMEPAGE_URL: str = "https://github.com/LAR/lincolnbeet_dataset"
 # e.g. "https://some.com/dataset/homepage"
 
 PREVIEW_IMAGE_ID: int = None
 # This should be filled AFTER uploading images to instance, just ID of any image.
 
-GITHUB_URL: str = None
+GITHUB_URL: str = "https://github.com/dataset-ninja/lincoln-beet"
 # URL to GitHub repo on dataset ninja (e.g. "https://github.com/dataset-ninja/some-dataset")
 
 ##################################
 ### * Optional after uploading ###
 ##################################
-DOWNLOAD_ORIGINAL_URL: Optional[Union[str, dict]] = None
+DOWNLOAD_ORIGINAL_URL: Optional[Union[str, dict]] = ["https://www.dropbox.com/s/0rq7cc8t6rja632/all_fields_lincolnbeet.zip?dl=0"]
 # Optional link for downloading original dataset (e.g. "https://some.com/dataset/download")
 
 CLASS2COLOR: Optional[Dict[str, List[str]]] = None
@@ -51,19 +51,19 @@ CLASS2COLOR: Optional[Dict[str, List[str]]] = None
 
 # If you have more than the one paper, put the most relatable link as the first element of the list
 # Use dict key to specify name for a button
-PAPER: Optional[Union[str, List[str], Dict[str, str]]] = None
+PAPER: Optional[Union[str, List[str], Dict[str, str]]] = ["https://arxiv.org/abs/2109.11048"]
 BLOGPOST: Optional[Union[str, List[str], Dict[str, str]]] = None
-REPOSITORY: Optional[Union[str, List[str], Dict[str, str]]] = {"GitHub":"some_link_to_repo_if_exists"}
+REPOSITORY: Optional[Union[str, List[str], Dict[str, str]]] = {"Kaggle":"https://www.kaggle.com/datasets/amiranmkrtchyan/amirans"}
 
-CITATION_URL: Optional[str] = None
-AUTHORS: Optional[List[str]] = None
+CITATION_URL: Optional[str] = "https://arxiv.org/abs/2109.11048"
+AUTHORS: Optional[List[str]] = ["Adrian Salazar-Gomez", "Madeleine Darbyshire", "Junfeng Gao", "Elizabeth I Sklar", "Simon Parsons"]
 AUTHORS_CONTACTS: Optional[List[str]] = None
 
-ORGANIZATION_NAME: Optional[Union[str, List[str]]] = None
-ORGANIZATION_URL: Optional[Union[str, List[str]]] = None
+ORGANIZATION_NAME: Optional[Union[str, List[str]]] = ["University of Lincoln, UK"]
+ORGANIZATION_URL: Optional[Union[str, List[str]]] = ["https://www.lincoln.ac.uk/"]
 
 # Set '__PRETEXT__' or '__POSTTEXT__' as a key with string value to add custom text. e.g. SLYTAGSPLIT = {'__POSTTEXT__':'some text}
-SLYTAGSPLIT: Optional[Dict[str, Union[List[str], str]]] = None
+SLYTAGSPLIT: Optional[Dict[str, Union[List[str], str]]] = {"__POSTTEXT__":"Also, the dataset contains ***item_density***, ***bbox_occupation***, ***average_relative_size*** and ***averege_level_occlusion*** image tags"}
 TAGS: Optional[List[str]] = None
 
 
